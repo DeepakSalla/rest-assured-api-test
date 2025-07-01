34a4d760dd566eafc4f8720fda9d5e71ab6b1b60
DeepakSalla <Salladeepak1@gmail.com>
2025-07-01 20:54:52 +0530

    Fix: Add protected ExtentReports methods to BaseTest

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,12 +1,37 @@
 package base;
 
 import io.restassured.RestAssured;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.*;
 import utils.ConfigReader;
+import utils.ExtentReportManager;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 
 public class BaseTest {
+
+    protected static ExtentReports extent;
+    protected static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    @BeforeSuite
+    public void setUpReport() {
+        extent = ExtentReportManager.getReporter();
+    }
+
     @BeforeClass
-    public void setup() {
+    public void setupRestAssured() {
         RestAssured.baseURI = ConfigReader.getBaseUrl();
+    }
+
+    @AfterMethod
+    public void flushTestReport() {
+        extent.flush();
+    }
+
+    protected void createTest(String testName) {
+        test.set(extent.createTest(testName));
+    }
+
+    protected ExtentTest getTest() {
+        return test.get();
     }
 }
